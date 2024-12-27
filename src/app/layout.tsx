cc2868885d75aa421529b8cccfd735540ab6beb8
Jamoliddin Samadov <gamalsamadov@gmail.com>
2024-12-27 12:12:17 +0300

    Description added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,8 @@ const spaceGrotesk = localFont({
 
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "Batter version of Stack over flow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
   icons: {
     icon: "/images/site-logo.svg",
   },
