eb845e283f6935a073309b22e211539894e83283
Jamoliddin Samadov <gamalsamadov@gmail.com>
2024-12-28 12:21:51 +0300

    Setup light & dark mode 🌓

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from "next/font/local";
 import React from "react";
 
 import "./globals.css";
+import { Navbar } from "@/components/navigations/navbar";
+import { ThemeProvider } from "@/context/ThemeProvider";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -31,11 +33,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
