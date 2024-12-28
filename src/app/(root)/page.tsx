f20b26fc827b962dc206e49fdf74dc8b283c89b0
Jamoliddin Samadov <gamalsamadov@gmail.com>
2024-12-28 16:15:09 +0300

    Authentication with Github using next-auth 🔐

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,27 @@
+import { signOut, auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+
+  console.log(session);
+  return (
+    <>
+      <h1 className="h1-bold">Salom dunyo</h1>
+      <h1 className="h1-bold font-inter">Salom dunyo</h1>
+
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Logout</Button>
+      </form>
+    </>
+  );
+};
+
+export default Home;
