c74cf776fa632f0dc0bc4a27a9bdbd2d651e83bc
Jamoliddin Samadov <gamalsamadov@gmail.com>
2024-12-29 10:17:23 +0300

    Mobile navigation 📱

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,4 @@
-import { signOut, auth } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
@@ -10,16 +8,6 @@ const Home = async () => {
     <>
       <h1 className="h1-bold">Salom dunyo</h1>
       <h1 className="h1-bold font-inter">Salom dunyo</h1>
-
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Logout</Button>
-      </form>
     </>
   );
 };
