c8cf540167e8a282a4155dc4fbf06439e7c8fa5d
Jamoliddin Samadov <gamalsamadov@gmail.com>
2025-01-02 11:45:15 +0300

    Implement edit and Get Question functionality

--- src/database/tag.model.ts
+++ src/database/tag.model.ts
@@ -1,8 +1,11 @@
-import { model, models, Schema } from "mongoose";
+import { model, models, Schema, Document } from "mongoose";
 export interface ITag {
   name: string;
   questions: number;
 }
+
+export interface ITagDoc extends ITag, Document {}
+
 const TagSchema = new Schema<ITag>(
   {
     name: { type: String, required: true, unique: true },
@@ -10,5 +13,7 @@ const TagSchema = new Schema<ITag>(
   },
   { timestamps: true }
 );
+
 const Tag = models?.Tag || model<ITag>("Tag", TagSchema);
+
 export default Tag;
